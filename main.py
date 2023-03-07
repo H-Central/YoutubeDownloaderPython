12fcf6d1c203dcf4fbbd78454af1d88d68b1fb31
Zerbaib <hugo.hennetin@proton.me>
2023-03-07 16:36:02 +0100

    done

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 from fileinput import filename
 from pytube import YouTube
+
+link = input("Set our video link:\n")
+format = input("What format you want: (1 - mp3) (2 - mp4)\n")
+yt = YouTube(link)
+
+if format != 1 or 2:
+    print("Our choice is wrong")
+
+def mp3():
+    downloader.download(filename="YTBaudio.mp3")
+def mp4():
+    downloader.download(filename="YTBvideo.mp4")
+
+downloader = yt.streams.get_highest_resolution()    
+print("Download please wait")
+
+if format == "1":
+    mp3()
+else:
+    mp4()
+
+print("finish")
+print(f"Check the result at {filename}")
